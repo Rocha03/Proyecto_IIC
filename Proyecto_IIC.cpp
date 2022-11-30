8db406b491b2c367f4f813448f6a28be29a156bc
Rocha03 <marianorocha03@gmail.com>
2022-11-30 15:48:19 -0600

    Primera modificación, solamente damos indicaciones de lo que hara el programa

---
+++ Proyecto_IIC.cpp
@@ -0,0 +1,11 @@
+/*Programa que usa dos funciones tanto para crear elaborar el factorial de un numero, y por otro lado
+ls segunda servira para ejecturar la serie de Fibbonaci(Esta suma un determinado numero 1 con el siguiente, en
+este caso 2, para hacer un bucle que evaluara tantos numeros como el usuario ingrese)
+ROCHA GARCÍA MARIANO
+GRUPO 1108*/
+#include<stdio.h>
+
+
+void main(void) {
+
+}
